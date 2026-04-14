de023cecc1283e65f40213456773c0945ec0dc77
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-04-14 23:49:47 +0900

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,18 +27,26 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <header>
-          <div>
-            <Link href={"/"}>Event Planner</Link>
-            <nav>
-              <Link href={"/dashboard"}>Dashboard</Link>
-            </nav>
-          </div>
-        </header>
-        {children}
+      <body className="min-h-full flex flex-col bg-background text-foreground">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <header className="border-b border-border bg-accent/80 backdrop-blur">
+            <div className="mx-auto flex h-16 w-full max-w-6xl items-center justify-between">
+              <Link href={"/"}>Event Planner</Link>
+              <nav>
+                <Link href={"/dashboard"}>Dashboard</Link>
+              </nav>
+            </div>
+          </header>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col flex-1 items-center justify-center">
       Home Page
+      <Button variant={"secondary"}>Click Me</Button>
+      <ModeToggle />
     </div>
   );
 }
